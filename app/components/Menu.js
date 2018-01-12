70d99e904a385733462914e4578afd08674a450e
System Administrator <root@Sergeys-iMac.local>
2018-01-12 10:14:08 -0800

    install vector icons

--- app/components/Menu.js
+++ app/components/Menu.js
@@ -6,14 +6,18 @@ import {
     Button
 } from 'react-native';
 
+import IconFA from './../../node_modules/react-native-vector-icons/FontAwesome';
+
 export default class ComponentMenu extends React.Component {
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.button}>
+                    <IconFA name="home" size={30} color="#900"/>
                     <Button title="One"/>
                 </View>
                 <View style={styles.button}>
+                    <IconFA name="rocket" size={30} color="#900"/>
                     <Button title="Two"/>
                 </View>
             </View>
