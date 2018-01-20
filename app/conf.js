08d9daf32b0f6fa549149e903a647bd520189bc7
System Administrator <root@Sergeys-iMac.local>
2018-01-19 16:41:02 -0800

    init language

--- app/conf.js
+++ app/conf.js
@@ -8,7 +8,7 @@ obj.prototypes = ( function() {
     String.prototype.t = function() {
         return ( loc[ obj.lang ] && loc[ obj.lang ][ this.toString() ] )
                     ? loc[ obj.lang ][ this.toString() ]
-                    : this;
+                    : this.toString();
     }
 })();
 
