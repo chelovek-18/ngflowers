d2629d3fbaffe912fd564bbb7b42630da6f709cf
serg <chelovek-18@yandex.ru>
2018-02-10 23:55:12 +0300

    Probe

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+'use strict';
 
+const
+	fCount = 3, //require( './config/config' ).forkCount,
+	cluster = require( 'cluster' );
+
+/* Форки (количество настраивается в конфиге). Распараллеливание запросов. */
+if ( cluster.isMaster ) {
+
+	for ( let i = 0; i < fCount; i++ )
+		cluster.fork();
+	cluster.on( 'exit', ( worker, code, signal ) => {
+		console.log( `Процесс ${worker.process.pid} прибит` );
+		setTimeout( function() {
+			cluster.fork();
+		}, 2000 );
+	});
+
+} else {
+
+	new ( require( './route' ) );
+	
+}
---
+++ route/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const
+    express = require( 'express' )
+    app = express();
+
+class Route
+{
+    constructor() {
+        app
+            .listen( 80, () => {
+                this.allRoutes();
+            });
+    }
+
+    allRoutes() {
+        app.use( function( req, res, next ) {
+            res.send( 'ok. fuck.' );
+        });
+    }
+}
+
+module.exports = Route;
