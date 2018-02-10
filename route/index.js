5af15396b7f5abc8b9b6d88396f49508009f1527
serg <chelovek-18@yandex.ru>
2018-02-11 02:28:52 +0300

    upd

--- route/index.js
+++ route/index.js
@@ -2,7 +2,19 @@
 
 const
     express = require( 'express' ),
-    app = express();
+    app = express(),
+    // fs
+    fs = require( 'fs' ),
+    dir = fs.readdirSync( __dirname + '/api', 'utf8' );
+
+let
+	routes = [];
+
+// Подключаем все js файлы из папки api
+dir.forEach( ( fnm ) => {
+	if ( fnm.split( '.' )[ 1 ] == 'js' ) routes.push( require( './api/' + fnm ) );
+});
+
 
 class Route
 {
@@ -14,12 +26,9 @@ class Route
     }
 
     allRoutes() {
-        app.use( '/q', function( req, res, next ) {
-            res.send( 'ok. fuck.' );
-        });
-
-        app.use( function( req, res, next ) {
-            throw 404;
+        // Все маршруты
+        routes.forEach( ( route ) => {
+            if ( route.use ) app.use( '/app/', route );
         });
     }
 }
