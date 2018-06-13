c593396ee0e7ffc9491f5693ee5e8e6548d52e31
serg <chelovek-18@yandex.ru>
2018-06-13 12:47:15 +0300

    ws probe

--- route/index.js
+++ route/index.js
@@ -3,6 +3,10 @@
 const
     express = require( 'express' ),
     app = express(),
+
+    server = http.createServer( app ),
+    WebSocket = require( 'ws' ),
+    wss = new WebSocket.Server({ server }),
     // model
     //model = new ( require( './../model/model' ) ),
     // fs
@@ -30,6 +34,13 @@ class Route
             .listen( 50001, () => {
                 this.allRoutes();
             });
+
+        server.listen( 50001 );
+        wss.on( 'connection', function connection( ws ) {
+            ws.on( 'message', function ( message ) {
+                ws.send( `ok, ${ message }` );
+            });
+        });
     }
 
     allRoutes() {
@@ -46,7 +57,68 @@ class Route
             <input type="text" /><br />
             <p>Пароль</p>
             <input type="password" /><br />
+            <script>
+                var socket = new WebSocket( 'ws://5.101.180.14:50001' );
+                socket.onopen = function() {
+                    console.log( 'ws open!' );
+                    setTimeout( () => {
+                        socket.send( 'proverka' );
+                    }, 1500);
+                }
+                socket.onmessage = function( message ) {
+                    console.log( 'ws message! ' + message );
+                }
+                socket.onerror = function( e ) {
+                    console.log( 'ws error! ' + e )
+                }
+                socket.onclose = function( e ) {
+                    console.log( 'ws close! ' + e )
+                }
+            </script>
             ` )
+        });
+        app.get( '/admin/', ( req, res, next ) => {
+            res.send(`
+            <ul style="float: left;">
+                <li><a href="#">Настройки</a></li>
+                <li><a href="#">Страницы</a></li>
+                <li><a href="#">Чат</a></li>
+                <li><a href="#">Активность</a></li>
+            </ul>
+            <div>
+                <h1>Настройки</h1>
+                <p>Порт</p>
+                <input type="text" value="50001" /><br />
+                <p>База</p>
+                <input type="text" value="ngflowers" /><br />
+                <p>Логин</p>
+                <input type="text" value="ngroot" /><br />
+                <p>Пароль</p>
+                <input type="password" value="ngpas" /><br />
+                <hr />
+                <h1>Пользователи</h1>
+                <table>
+                    <tr>
+                        <td> Имя </td>
+                        <td> Логин </td>
+                        <td> Пароль </td>
+                        <td> Роль </td>
+                    </tr>
+                    <tr>
+                        <td> <input type="text" value="Сергей" /> </td>
+                        <td> <input type="text" value="serg007" /> </td>
+                        <td> <input type="password" value="ngr" /> </td>
+                        <td> <select><option>Root</option><option>Admin</option></select> </td>
+                    </tr>
+                    <tr>
+                        <td> <input type="text" value="Антон" /> </td>
+                        <td> <input type="text" value="anton008" /> </td>
+                        <td> <input type="password" value="ngr" /> </td>
+                        <td> <select><option>Root</option><option>Admin</option></select> </td>
+                    </tr>
+                </table>
+            </div>
+            `);
         });
     }
 }
