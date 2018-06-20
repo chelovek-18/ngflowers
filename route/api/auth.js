2eca564ae38cc9dcbc02ba8baebf2bd983b711a5
serg <chelovek-18@yandex.ru>
2018-06-20 17:23:58 +0300

    bug #2

--- route/api/auth.js
+++ route/api/auth.js
@@ -11,8 +11,8 @@ router.use( ( req, res, next ) => {
             req.body.login
             && req.body.login == 'admin'
         ) {
+            req.session.user = req.body.login;
             res.send( req.body.login );
-            //req.session.user = req.body.login;
             //res.redirect( '/' );
         } else res.render( 'partials/login' );
     }
