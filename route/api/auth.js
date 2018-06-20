7a9bfb2624f669ebb13e4607f58863fc4d742676
serg <chelovek-18@yandex.ru>
2018-06-20 17:57:55 +0300

    bug #5

--- route/api/auth.js
+++ route/api/auth.js
@@ -6,6 +6,7 @@ const
 
 // ------------------------------------- Авторизация -------------------------------------
 router.use( ( req, res, next ) => {
+    res.send( req.body.login + ' ?? ' + req.session.user );
     if ( !req.session.user ) {
         if (
             req.body.login
