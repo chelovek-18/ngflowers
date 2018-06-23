a6e6ceec567eca0bb2c90cf53935528f74fb03f9
serg <chelovek-18@yandex.ru>
2018-06-23 23:58:53 +0300

    admin page bug fixed #5

--- route/api/admin.js
+++ route/api/admin.js
@@ -14,7 +14,8 @@ router.use( ( req, res, next ) => {
 });
 
 router.get( '/', ( req, res, next ) => {
-    res.render( 'partials/page', req.pageSettings( 'settings' ) );
+    res.send( 'WTF?????' );
+    //res.render( 'partials/page', req.pageSettings( 'settings' ) );
 });
 //router.get( '/', require( './admin0/settings' ) );
 
