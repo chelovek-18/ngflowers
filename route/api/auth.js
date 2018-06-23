1636dfd3e386698a57a780c9f8f074f47a47be93
serg <chelovek-18@yandex.ru>
2018-06-23 15:36:09 +0300

    db await find() experiment

--- route/api/auth.js
+++ route/api/auth.js
@@ -6,7 +6,7 @@ const
 
 // ------------------------------------- Авторизация -------------------------------------
 router.use( async ( req, res, next ) => {
-    console.log( 'db:', req.db.users().find().exec() );
+    console.log( 'db:', await req.db.users().find().exec() );
     if ( !req.session.user ) {
         if (
             req.body.login
