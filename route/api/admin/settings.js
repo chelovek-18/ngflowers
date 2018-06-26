e323a537c8bbee47ab348f86f6dc3ff110089a5a
serg <chelovek-18@yandex.ru>
2018-06-26 18:21:06 +0300

    change menu #5

--- route/api/admin/settings.js
+++ route/api/admin/settings.js
@@ -7,7 +7,7 @@ const
 // ------------------------------------- Настройки -------------------------------------
 router.use( ( req, res, next ) => {
     res.pageSettings.page = 'settings';
-    res.pageSettings.menu = res.pageSettings.menu.map( m => { if ( m.key == res.pageSettings.page ) m.actived = true; return m; });
+    //res.pageSettings.menu = res.pageSettings.menu.map( m => { if ( m.key == res.pageSettings.page ) m.actived = true; return m; });
     if ( !req.access() ) throw 401;
 
     next();
