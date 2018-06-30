939a57e45eeda646b1de753b6d51dc6a99bfa369
serg <chelovek-18@yandex.ru>
2018-07-01 01:12:38 +0300

    add form header

--- public/js/admin.js
+++ public/js/admin.js
@@ -5,6 +5,7 @@ document.addEventListener( "DOMContentLoaded", function() {
         this.classList.toggle( 'use-true' );
         this.classList.toggle( 'use-false' );
         var xhr = new XMLHttpRequest();
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xhr.open( 'POST', location.origin + '/' + this.getAttribute( 'type' ) + '/' + this.getAttribute( 'key' ) );
         xhr.send( dataSerialize( { use: this.classList.contains( 'use-true' ) } ) );
     });
