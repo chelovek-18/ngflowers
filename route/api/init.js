857dcfec09b5b9d99961ec2c8a6b7f508403d349
serg <chelovek-18@yandex.ru>
2018-06-30 16:36:41 +0300

    bug experiment #6

--- route/api/init.js
+++ route/api/init.js
@@ -5,16 +5,22 @@ const
     router = express.Router(),
     
     model = new ( require( './../../model/model' ) ),
-    //ng = new ( require( './../../libs/ng' ) ),
+    ng = new ( require( './../../libs/ng' ) ),
     refreshDatas = async () => {
-        //let
-            //rCities = await ng.getCities();
+        let
+            rCities = ( () => {
+                try {
+                    return await ng.getCities();
+                } catch ( err ) {
+                    return [ 789 ];
+                }
+            })();
         /*cities.forEach( c => {
             if ( !rCities[ c.key ] ) {
                 model.cities().update( { used: false }, { key: c.key } );
             }
         });*/
-        console.log( 'cities:'/*, rCities*/ );
+        console.log( 'cities:', rCities );
     };
 
 let
