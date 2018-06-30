c72102c20b57829aac7259cc4b5f42b50c6784f6
serg <chelovek-18@yandex.ru>
2018-07-01 00:32:11 +0300

    frontend switcher

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,8 +2,8 @@ document.addEventListener( "DOMContentLoaded", function() {
     document.querySelectorAll( '.use-switcher' ).forEach( e => e.onclick = function() {
         this.classList.toggle( 'use-true' );
         this.classList.toggle( 'use-false' );
-        if ( this.classList.contains( 'use-true' ) ) {
-        } else {
-        }
+        var xhr = new XMLHttpRequest();
+        xhr.open( 'POST', location.origin + '/' + this.type + '/' + this.key );
+        xhr.send( { use: this.classList.contains( 'use-true' ) } );
     });
 });
