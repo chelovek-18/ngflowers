6b5488f86037463ef08f017428ca3b6efb54a6d9
serg <chelovek-18@yandex.ru>
2018-06-30 23:48:04 +0300

    switch city

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,8 @@
-document.querySelectorAll( '.use-switcher' ).forEach( e => e.onclick = function() {
-    if ( this.classList.contains( 'use-true' ) )
-        this.classList.remove( 'use-true' ).add( 'use-false' );
+document.addEventListener( "DOMContentLoaded", function() {
+    document.querySelectorAll( '.use-switcher' ).forEach( e => e.onclick = function() {
+        if ( this.classList.contains( 'use-true' ) ) {
+            this.classList.remove( 'use-true' );
+            this.classList.add( 'use-false' );
+        }
+    });
 });
