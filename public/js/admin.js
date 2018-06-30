8e7dcad0cdc1b321beb7fb950a9804178bd320bc
serg <chelovek-18@yandex.ru>
2018-06-30 23:42:05 +0300

    city name request #21

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,4 @@
-document.querySelector( '.use-switcher' ).onClick = function() {
-    alert( 'bebebe!' );
-}
+document.querySelectorAll( '.use-switcher' ).forEach( e => e.onclick = function() {
+    if ( this.classList.contains( 'use-true' ) )
+        this.classList.remove( 'use-true' ).add( 'use-false' );
+});
