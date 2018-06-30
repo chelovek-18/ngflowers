acd7c153a47ecda68ee5bf09630bf06fbe05380d
serg <chelovek-18@yandex.ru>
2018-07-01 01:06:35 +0300

    serialize put-data

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,9 +1,11 @@
 document.addEventListener( "DOMContentLoaded", function() {
+    var dataSerialize = function( obj ) { return Object.keys( obj ).map( k => k + '=' + obj[ k ] ).join( '&' ); };
+
     document.querySelectorAll( '.use-switcher' ).forEach( e => e.onclick = function() {
         this.classList.toggle( 'use-true' );
         this.classList.toggle( 'use-false' );
         var xhr = new XMLHttpRequest();
-        xhr.open( 'POST', location.origin + '/' + this.getAttribute( 'type' ) + '/' + this.getAttribute( 'key' ) );
-        xhr.send( { use: this.classList.contains( 'use-true' ) } );
+        xhr.open( 'PUT', location.origin + '/' + this.getAttribute( 'type' ) + '/' + this.getAttribute( 'key' ) );
+        xhr.send( dataSerialize( { use: this.classList.contains( 'use-true' ) } ) );
     });
 });
