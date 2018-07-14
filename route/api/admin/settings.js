3ae77edb4b6da7dc262c08971b89a4d7ad4caf20
serg <chelovek-18@yandex.ru>
2018-07-15 02:43:12 +0300

    model error exp #6

--- route/api/admin/settings.js
+++ route/api/admin/settings.js
@@ -17,7 +17,7 @@ router.use( async ( req, res, next ) => {
 
 
 router.get( '/', ( req, res, next ) => {
-    console.log( 'eee1!' );
+    console.log( 'eee3!' );
     res.send( 'faka!))' );
     //res.render( 'partials/page', res.pageSettings );
 });
