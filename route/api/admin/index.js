2f591d86ca4e6ea2e33c4b637f45f49da703db8f
serg <chelovek-18@yandex.ru>
2018-07-15 04:25:27 +0300

    model error exp #21

--- route/api/admin/index.js
+++ route/api/admin/index.js
@@ -6,8 +6,8 @@ const
     
     //fs = require( 'fs' );
 
-let
-    conf = require( './../../../config/config' );
+//let
+    //conf = require( './../../../config/config' );
 
 // ------------------------------------- Админка -------------------------------------
 router.use( ( req, res, next ) => {
@@ -22,14 +22,14 @@ router.use( ( req, res, next ) => {
 });
 
 router.post( '/settings/db/update/', ( req, res, next ) => {
-    //let conf = require( './../../config/config' );
-    //conf.mongodb.port = req.body.port;
-    //conf.mongodb.database = req.body.database;
-    //conf.mongodb.user = req.body.user;
-    //conf.mongodb.password = req.body.password;
-    //fs.writeFileSync( global.appConf.location.root + '/config/config.json', JSON.stringify( conf ) );
-    //res.redirect( '/' );
-    res.send( 'q' );
+    let conf = require( './../../../config/config' );
+    conf.mongodb.port = req.body.port;
+    conf.mongodb.database = req.body.database;
+    conf.mongodb.user = req.body.user;
+    conf.mongodb.password = req.body.password;
+    fs.writeFileSync( global.appConf.location.root + '/config/config.json', JSON.stringify( conf ) );
+    res.redirect( '/' );
+    //res.send( 'q' );
 });
 
 router.use( ( req, res, next ) => {
