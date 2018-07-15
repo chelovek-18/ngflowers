6b59f2522529a4ce9b3475576ebf7a8ef50bda8a
serg <chelovek-18@yandex.ru>
2018-07-15 04:56:20 +0300

    model error exp #25

--- route/api/admin/index.js
+++ route/api/admin/index.js
@@ -28,6 +28,7 @@ router.post( '/settings/db/update/', ( req, res, next ) => {
     conf.mongodb.user = req.body.user;
     conf.mongodb.password = req.body.password;
     fs.writeFileSync( global.appConf.location.root + '/config/config.json', JSON.stringify( conf ) );
+    res.send( '<h1>Сервер перезапускается...</h1>' );
     process.exit();
     //res.redirect( '/' );
     //res.send( 'q' );
