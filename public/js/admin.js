ff1cde2600c709a690f81da03423cf5821ba4705
serg <chelovek-18@yandex.ru>
2018-07-15 23:22:28 +0300

    user btn function global

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,42 +1,47 @@
-document.addEventListener( "DOMContentLoaded", function() {
-    alert( 'hero!' );
-    // Функционал
-    var Ajax = function() {
-        var self = this;
-        this.method = 'GET';
-        [ 'get', 'post', 'put', 'delete' ].forEach( function( m ) {
-            self[ m ] = function() {
-                self.method = m.toUpperCase();
-                return self;
-            }
-        });
-        [ 'data', 'path' ].forEach( function( m ) {
-            self[ m ] = function( data ) {
-                self[ '_' + m ] = data;
-                return self;
-            }
-        });
-        this.send = function() {
-            var xhr = new XMLHttpRequest();
-            xhr.open(
-                self.method,
-                location.origin + self._path,
-                true
-            );
-            xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
-            xhr.send( dataSerialize( self._data ) );
-            return new Promise( function( r, j ) {
-                xhr.onreadystatechange = function() {
-                    if ( xhr.readyState != 4 ) return;
-
-                    if ( xhr.status != 200 ) {
-                        j( xhr );
-                    } else r( xhr );
-                }
-            });
+// Функционал
+var Ajax = function() {
+    var self = this;
+    this.method = 'GET';
+    [ 'get', 'post', 'put', 'delete' ].forEach( function( m ) {
+        self[ m ] = function() {
+            self.method = m.toUpperCase();
+            return self;
         }
+    });
+    [ 'data', 'path' ].forEach( function( m ) {
+        self[ m ] = function( data ) {
+            self[ '_' + m ] = data;
+            return self;
+        }
+    });
+    this.send = function() {
+        var xhr = new XMLHttpRequest();
+        xhr.open(
+            self.method,
+            location.origin + self._path,
+            true
+        );
+        xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+        xhr.send( dataSerialize( self._data ) );
+        return new Promise( function( r, j ) {
+            xhr.onreadystatechange = function() {
+                if ( xhr.readyState != 4 ) return;
+
+                if ( xhr.status != 200 ) {
+                    j( xhr );
+                } else r( xhr );
+            }
+        });
     }
-    var dataSerialize = function( obj ) { return Object.keys( obj ).map( k => k + '=' + obj[ k ] ).join( '&' ); };
+}
+var dataSerialize = function( obj ) { return Object.keys( obj ).map( k => k + '=' + obj[ k ] ).join( '&' ); };
+
+function addUser( elm, tr ) {
+    alert( ( new Ajax ).post().data( { u: 77 } ).path( '/users/create' ).send() );
+
+    //var tr = document.createElement('tr'); tr.innerHTML = `{{getTr}}`; this.parentNode.parentNode.parentNode.insertBefore( tr, this.parentNode.parentNode );
+}
+document.addEventListener( "DOMContentLoaded", function() {
 
     document.querySelectorAll( '.use-switcher' ).forEach( e => e.onclick = function() {
         this.classList.toggle( 'use-true' );
@@ -47,9 +52,4 @@ document.addEventListener( "DOMContentLoaded", function() {
         xhr.send( dataSerialize( { use: this.classList.contains( 'use-true' ) } ) );
     });
 
-    function addUser( elm, tr ) {
-        alert( ( new Ajax ).post().data( { u: 77 } ).path( '/users/create' ).send() );
-
-        //var tr = document.createElement('tr'); tr.innerHTML = `{{getTr}}`; this.parentNode.parentNode.parentNode.insertBefore( tr, this.parentNode.parentNode );
-    }
 });
