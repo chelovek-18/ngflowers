60517a165dec01f648915b22b1ef8f85179b7141
serg <chelovek-18@yandex.ru>
2018-07-15 04:34:19 +0300

    model error exp #23

--- route/api/admin/index.js
+++ route/api/admin/index.js
@@ -28,7 +28,7 @@ router.post( '/settings/db/update/', ( req, res, next ) => {
     conf.mongodb.user = req.body.user;
     conf.mongodb.password = req.body.password;
     fs.writeFileSync( global.appConf.location.root + '/config/config.json', JSON.stringify( conf ) );
-    process.exit();
+    process.close();
     res.redirect( '/' );
     //res.send( 'q' );
 });
