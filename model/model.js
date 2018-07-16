a7bb9a2f325e6b5dc1ea747d6aaaf81cdd24dacc
serg <chelovek-18@yandex.ru>
2018-07-16 22:45:34 +0300

    get banners from city #3

--- model/model.js
+++ model/model.js
@@ -29,7 +29,7 @@ mongoose.Promise = global.Promise;
 // Схемы
 let models = dir.reduce(
 	( o, f ) => {
-		if ( f.split( '.' )[ 1 ] != 'js' ) return;
+		if ( f.split( '.' )[ 1 ] != 'js' ) return o;
 		o[ f.slice( 0, -3 ) ] = ( new ( require( './schema/' + f ) ) ); return o;
 	}, {});
 
