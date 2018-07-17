bb14a714958f15d74c9c1b0da5caf57b68845f51
serg <chelovek-18@yandex.ru>
2018-07-17 17:33:17 +0300

    update interval #2

--- route/api/init.js
+++ route/api/init.js
@@ -30,7 +30,7 @@ const
                 rKeysNew = reqCities.filter( c => !~keys.in.indexOf( c.key ) );
 
             // 1. Отключаем те города, что отключены в API
-            for ( let k in cities.filter( c => ~keys.out.indexOf( c.key ) ) ) {
+            /*for ( let k in cities.filter( c => ~keys.out.indexOf( c.key ) ) ) {
                 await model.cities().update( { key: cities[ k ].key }, { use: false } );
                 isUpd = true;
             }
@@ -57,7 +57,7 @@ const
             }
             
             // 4. Сохраняем в cities
-            if ( isUpd ) cities = await model.cities().find();
+            if ( isUpd ) cities = await model.cities().find();*/
 
         }
 
@@ -77,75 +77,6 @@ const
         }
         if ( geoUpd ) cities = await model.cities().find();
 };
-
-
-
-    // Периодическое обновление данных
-    /*refreshDatas = async () => {
-        // Данные из базы и из запроса для сравнения
-        cities = await cities;
-        let
-            rCities = await ng.getCities();
-
-        // Если не ошибка запроса, то:
-        if ( Object.keys( rCities ).length ) {
-            // 1. Фильтруем, оставляя только те города что есть в запросе
-            await model.cities().update( { use: false }, { key: 'spb' } ).exec();
-            return;
-            cities = cities.map( c => {
-                if ( !rCities[ c.key ] ) {
-                    // Лишние - отключаем
-                    model.cities().update( { use: false }, { key: c.key } );
-                } else if ( c.name != rCities[ c.key ].name || c.link != rCities[ c.key ].link || c.siteId != rCities[ c.key ].site_id ) {
-                    // Измененные - обновляем
-                    c.name = rCities[ c.key ].name;
-                    c.link = rCities[ c.key ].link;
-                    c.siteId = rCities[ c.key ].site_id;
-                    model.cities().update( c, { key: c.key } );
-                }
-                return c;
-            }).filter( c => rCities[ c.key ] );
-            let keys = cities.map( c => c.key );
-            // 2. Добавляем те, что нет у нас
-            let newCities = Object.keys( rCities ).filter( k => !~keys.indexOf( k ) );
-            //let nC = [];
-            for ( let nc in newCities ) {
-                let k = newCities[ nc ];
-                let c = rCities[ k ];
-                let city = await model.cities().findOne( { key: c.key } );
-                if ( !city ) {
-                    city = {
-                        key: c.key,
-                        name: c.name,
-                        link: c.link,
-                        siteId: c.site_id
-                    };
-                    model.cities().save( city );
-                    cities.push( city );
-                }
-            }*/
-                                /*-------------------------------cities = cities.concat(
-                                    Object.keys( rCities )
-                                        .filter( k => !~keys.indexOf( k ) )
-                                        .map( k => {
-                                            let city = {
-                                                key: k,
-                                                name: rCities[ k ].name,
-                                                link: rCities[ k ].link,
-                                                siteId: rCities[ k ].site_id
-                                            };
-                                            ( async () => {
-                                                if ( await model.cities().findOne( { key: city.key } ) )
-                                                    model.cities().update( Object.assign( city, { use: true } ), { key: city.key } );
-                                                else
-                                                    model.cities().save( city );
-
-                                            })();
-                                            return city;
-                                        })
-                                );-------------------------------*/
-        /*}
-    };*/
 
 // Данные
 let
