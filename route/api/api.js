2a20b33635cc2886a214c63a9790ef56b94a26e9
serg <chelovek-18@yandex.ru>
2018-07-18 00:43:10 +0300

    more icon

--- route/api/api.js
+++ route/api/api.js
@@ -7,7 +7,11 @@ const
 
 // ------------------------------------- API -------------------------------------
 router.get( '/cities/', ( req, res, next ) => {
-    res.json( req.cities.filter( c => c.use ).map( c => { return { key: c.key, name: c.name, link: c.link, location: c.location, banners: c.banners }; } ) );
+    res.json(
+        req.cities
+            .filter( c => c.use )
+            .map( c => { return { key: c.key, name: c.name, link: c.link, location: c.location }; } )
+    );
 });
 
 /*router.get( '/citiesgeo/', async ( req, res, next ) => {
