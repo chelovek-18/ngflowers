5af6bdaef283f53be38e527ac6ef9b57caec1885
serg <chelovek-18@yandex.ru>
2018-07-23 13:35:13 +0300

    remove https from link #2

--- route/api/init.js
+++ route/api/init.js
@@ -124,7 +124,7 @@ const
                                                     .cover( 300, 300 )
                                                     .write( imgpath );
                                             }); }*/
-                                            {console.log( 'туц', city.link, img.replace( '/resize_cache/', '/' ).replace( '/80_80_1/', '/' ) );
+                                            {console.log( 'туц', city.link.replace( 'https://', '' ), img.replace( '/resize_cache/', '/' ).replace( '/80_80_1/', '/' ) );
                                             console.log( 'img buf', await ( new images( city.link.replace( 'https://', '' ), img.replace( '/resize_cache/', '/' ).replace( '/80_80_1/', '/' ) ) ).getImage() );
                                             gm( await ( new images( city.link.replace( 'https://', '' ), img.replace( '/resize_cache/', '/' ).replace( '/80_80_1/', '/' ) ) ).getImage(), fnm )
                                                 .resize( 300, 300 )
