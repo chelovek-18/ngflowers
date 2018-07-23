8be0935f1056507c2e6971f725f5265d24e14133
serg <chelovek-18@yandex.ru>
2018-07-23 23:03:39 +0300

    img pipe

--- route/api/api.js
+++ route/api/api.js
@@ -5,6 +5,7 @@ const
     router = express.Router(),
     ng = new ( require( './../../libs/ng' ) ),
     geo = new ( require( './../../libs/geo' ) ),
+    https = require( 'https' ),
     fs = require( 'fs' ),
     url = require( 'url' ),
     images = new( require( './../../libs/images' ) );
@@ -72,8 +73,14 @@ router.get( '/rbanners/:city', async ( req, res, next ) => {
 });
 
 router.get( '/imgs/', async ( req, res, next ) => {
+    let imgsource = 'https://novayagollandiya.ru/upload/iblock/c65/c657526991c0ab9cfd65fb1edcb843ba.jpg';
+    https.get( imgsource, function( resp ) {
+        resp.pipe( fs.createWriteStream( `${ global.appConf.location.root }/public/prob.jpg` ) );
+        res.send( 'oke' );
+    });
+
     //res.end( fs.readFileSync( 'https://novayagollandiya.ru/upload/iblock/c65/c657526991c0ab9cfd65fb1edcb843ba.jpg' ) );
-    res.end( fs.readFileSync( await images.getImage( 'novayagollandiya.ru', '/upload/iblock/c65/c657526991c0ab9cfd65fb1edcb843ba.jpg' ) ) );
+    //res.end( fs.readFileSync( await images.getImage( 'novayagollandiya.ru', '/upload/iblock/c65/c657526991c0ab9cfd65fb1edcb843ba.jpg' ) ) );
 
 
 
