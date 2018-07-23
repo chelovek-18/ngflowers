bb2478c9e5019381463eb52cc983dd6f697ac616
serg <chelovek-18@yandex.ru>
2018-07-23 15:08:23 +0300

    read img file

--- route/api/init.js
+++ route/api/init.js
@@ -130,7 +130,7 @@ const
                                             console.log( 'тыц', imghttp );
                                             //console.log( 'img buf', await images.getImage( city.link.replace( 'https://', '' ), img.replace( '/resize_cache/', '/' ).replace( '/80_80_1/', '/' ) ) );
                                             //gm( await images.getImage( city.link.replace( 'https://', '' ), img.replace( '/resize_cache/', '/' ).replace( '/80_80_1/', '/' ) ), fnm )
-                                            gm( imghttp )
+                                            gm( fs.readFileSync( imghttp ) )
                                                 .resize( 300, 300 )
                                                 .write( imgpath, ( err ) => {
                                                     if ( err ) console.log( 'no done!', err );
