2ace8def61036bc50443d26b14a214cb17dea65a
serg <chelovek-18@yandex.ru>
2018-07-25 12:34:55 +0300

    save and resize process one thread

--- libs/images.js
+++ libs/images.js
@@ -5,6 +5,7 @@ const
     fs = require( 'fs' ),
     jimp = require( 'jimp' );
 
+let onProcess = false;
 
 class Images extends Request
 {
@@ -20,13 +21,15 @@ class Images extends Request
         return new Promise( async ( r, j ) => {
             let resp = await this.setBody().request();
             resp.pipe( fs.createWriteStream( imgpath ) );
-            console.log( 'image saved' );
+            console.log( 'image saved ' + imgpath );
             // Таймаут между запросами
             setTimeout( () => {
-                jimp.read( imgpath ).then( function ( img ) {
+                if ( ~fnm.indexOf( '.gif' ) ) {
+                    r();
+                } else jimp.read( imgpath ).then( function ( img ) {
                     img.resize( 600, jimp.AUTO ).write( imgpath.replace( fnm, fnm.replace( '.', '-1.' ) ) );
                     img.resize( 300, jimp.AUTO ).write( imgpath.replace( fnm, fnm.replace( '.', '-2.' ) ) );
-                    console.log( 'image resize' );
+                    console.log( 'image resize ' + imgpath );
                     r();
                 }).catch( function( err ) {
                     console.log( 'resize error', err );
@@ -39,6 +42,8 @@ class Images extends Request
     }
 
     async imgsExistenceCheck( city, prop ) {
+        if ( onProcess ) return;
+        onProcess = true;
         let self = this;
         let dirpath = `${ global.appConf.location.root }/public/thumbs/${ city.key }`;
         for ( let p in city[ prop ].filter( i => i.use ) ) {
@@ -60,9 +65,10 @@ class Images extends Request
                         fs.mkdirSync( dir );
                 });
                 if ( !fs.existsSync( imgpath ) || !fs.existsSync( imgpath.replace( fnm, fnm1 ) ) || !fs.existsSync( imgpath.replace( fnm, fnm2 ) ) )
-                    if ( !~fnm.indexOf( '.gif' ) ) await self.getImage( city.link, img, imgpath, fnm );
+                    await self.getImage( city.link, img, imgpath, fnm );
             }
         }
+        onProcess = false;
     }
 
 }
