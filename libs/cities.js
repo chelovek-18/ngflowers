aeefca36c0989ee3a6793b9462be704471d13b12
serg <chelovek-18@yandex.ru>
2018-07-28 19:00:11 +0300

    update item log

--- libs/cities.js
+++ libs/cities.js
@@ -88,6 +88,7 @@ class Cities extends Array
                     // Сравниваем значения и корректируем
                     Object.keys( rItem ).forEach( p => {
                         if ( rItem[ p ] != item[ p ] ) {
+                            global.log( 'upd', rItem, item );
                             item[ p ] = rItem[ p ];
                             propUpd = true;
                         }
