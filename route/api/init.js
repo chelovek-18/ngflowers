ca2f45d0b5daae6f2e521626242cc457cd0f024e
serg <chelovek-18@yandex.ru>
2018-08-11 15:48:19 +0300

    getmaxvers function

--- route/api/init.js
+++ route/api/init.js
@@ -22,11 +22,24 @@ async function dbcomplete() {
     global.log( 'База инициализирована' );
     // Получаем из базы города со всем, что к ним относится
     cities = ( await model.cities().find() ) || [];
+
     global.obj = {};
+    global.obj.getMaxVers = async () => {
+        return ( await model.settings().find() )
+            .map( s => s.version )
+            .sort( ( versA, versB ) => {
+                let a = versA.split( '.' ), b = versB.split( '.' );
+                if ( a[ 0 ] != b[ 0 ] ) return a[ 0 ] > b[ 0 ] ? 1 : -1;
+                if ( a[ 1 ] != b[ 1 ] ) return a[ 1 ] > b[ 1 ] ? 1 : -1;
+                if ( a[ 2 ] != b[ 2 ] ) return a[ 2 ] > b[ 2 ] ? 1 : -1;
+                return 0
+            }).pop() || '1.0.0';
+    }
     // Геттер и сеттер для cities в global
     Object.defineProperty( global.obj, 'cities', {
         get: () => cities,
         set: async cs => {
+            if ( !( cs instanceof Array ) ) cs = [ cs ];
             for( let k in cs )
                 await model.cities().update( { key: cs[ k ].key }, cs[ k ] );
             cities = await model.cities().find();
@@ -39,7 +52,7 @@ async function dbcomplete() {
     });
 
     // Получаем из базы и устанавливаем для геолокации настройки
-    let settings = await model.settings().findOne();
+    let settings = await model.settings().findOne( { version: await global.obj.getMaxVers() } );
     if ( !settings ) await model.settings().save( settings = { version: '1.0.0', phone: '', googleKey: geo.defaultData.key } );
     geo.setParams( settings );
 
