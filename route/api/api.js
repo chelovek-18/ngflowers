1c3f873a6ab7595a41569c26391f17766e544ccf
serg <chelovek-18@yandex.ru>
2018-08-12 15:39:04 +0300

    gif slide 2 offset

--- route/api/api.js
+++ route/api/api.js
@@ -97,10 +97,10 @@ router.get( '/ci/', async ( req, res, next ) => {
 router.get( '/xgif/', async ( req, res, next ) => {
     GifUtil.read( global.appConf.location.root + '/public/proba.gif' ).then( inputGif => {
         global.log( "gif!! width/height", inputGif.width, inputGif.height );
-        inputGif.frames = inputGif.frames.filter( ( f, i ) => !i );
+        inputGif.frames = inputGif.frames.filter( ( f, i ) => i == 1 );
         inputGif.frames.forEach( frame => {
             global.log( "gif width/height", frame.bitmap.width, frame.bitmap.height );
-            frame.scale( 0.5 ); //reframe( 0, 0, 904, 377 );
+            reframe( 86, 0, 904, 377 );
         });
         return GifUtil.write( global.appConf.location.root + '/public/outed.gif', inputGif.frames, inputGif ).then( outputGif => {
             res.send( 'Вроде е...' );
