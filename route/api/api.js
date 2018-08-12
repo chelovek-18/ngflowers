f5f1f8df6ab2a470f58902c43c1a1c1b1bc22f40
serg <chelovek-18@yandex.ru>
2018-08-12 19:33:22 +0300

    get vers

--- route/api/api.js
+++ route/api/api.js
@@ -70,7 +70,7 @@ router.get( '/app-settings/:version', async ( req, res, next ) => {
     req.session.appVersion = req.params.version;
     let settings = await req.db.settings().findOne( { version: req.session.appVersion } );
     if ( !settings ) settings = await req.db.settings().findOne( { version: await global.obj.getMaxVers() } );
-    settings = Object.assign( {}, settings );
+    let vers = settings.version;
     settings.isCurrent = req.params.version == await global.obj.getMaxVers();
     res.json( settings );
 });
