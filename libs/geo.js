cedd16bece7536e5102ddff7b3c4fc390d31761c
serg <chelovek-18@yandex.ru>
2018-08-15 15:51:20 +0300

    remove google key

--- libs/geo.js
+++ libs/geo.js
@@ -12,7 +12,7 @@ class Geo extends Request
         this.host = 'maps.google.com';
         this.defaultPath = '/maps/api/geocode/json';
         this.method = 'GET';
-        this.defaultData = { sensor: false, key: 'AIzaSyAQc5Tfg8shWq24eTkwWzshLG0p58ZLH7M' };
+        this.defaultData = { sensor: false, key: '' };
         this.address = 'Санкт-Петербург';
     }
 
